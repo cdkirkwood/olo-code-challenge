c9b49c2be5c0c4e105e9819fc8dd2bacf70ab909
Connor Kirkwood <cdkirkwood@gmail.com>
2018-07-10 00:08:11 -0400

    reformatted output

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ const axios = require('axios')
 const calculateMostPopular = table => {
   const tableArr = Object.entries(table)
   const sortedArr = tableArr.sort((first, second) => second[1] - first[1])
-  return sortedArr.slice(0, 10)
+  const topTenToppings = sortedArr.slice(0, 20)
+  return topTenToppings.reduce((finalArray, curTopping, index) => {
+    const toppingsString = curTopping[0]
+    const newObj = {
+      rank: index + 1,
+      toppings: toppingsString.split(','),
+      populaity: curTopping[1]
+    }
+    finalArray.push(newObj)
+    return finalArray
+  }, [])
 }
 
 const generateHashTable = orders => {
